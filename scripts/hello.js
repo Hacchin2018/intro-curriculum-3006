3a5d837bfe33d402c03165097ffa78ed8396008f
Hacchin2018 <sshiro2010@gmail.com>
2020-06-04 15:25:48 +0900

    おみくじ修正2

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,9 +1,16 @@
 'use strict';
+module.exports = robot => {
+	robot.hear(/hello>/i, msg => {
+		const user_id = msg.message.user.id;
+		msg.send(`Hello,<@${user_id}>`);
+	});
+};
+
 module.exports = (robot) => {
 	robot.hear(/おみくじ>/i, (msg) => {
-		const username = msg.message.user.name;
+		const user_id = msg.message.user.id;
 		const lots = ['大吉', '基地', '忠吉', '末吉', '経'];
 		const lot = lots[Math.floor(Math.random() * lots.length)];
-		msg.send(lot + ', ' + username);
+		msg.send(`${lot}, <@${user_id}>`);
 	});
 };
